d73de6dd462cc7f15fcbb5612b8e565154c2e7f9
Luis Paz <lpaz8806@gmail.com>
2022-12-06 10:12:21 +0100

    Update MainActivity.java

--- app/src/main/java/se/handelsakademin/mybookstore/MainActivity.java
+++ app/src/main/java/se/handelsakademin/mybookstore/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void whatever(){
-
+    private void whatever() {
+    }
 
     private void zoo(){
 
